dec4592d734f68ce413914cd6e7f5ee767b1b1ff
884yuu <ytkbys@gmail.com>
2018-01-17 01:57:22 +0000

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 'use strict';
 const number = process.argv[2] || 0;
+console.log("nodeのパスは：" + process.argv[0]);
+console.log("ファイルのパスは：" + process.argv[1]);
+
+if (number>= 0){
+console.log("0から" + process.argv[2] + "までの合計は↓");
 let sum = 0;
 for (let i = 1; i <= number; i++){
     sum = sum + i;
 }
 console.log(sum);
+}else{
+console.log("自然数を入力してください");
+}
+
+
